daa310e68a70d6228681b98417e2e78d9e16fe1e
Bram <brampijper@gmail.com>
2019-09-12 17:33:50 +0200

    Implemented hello world to test webpack setup

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1> Hello World! </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+    h1 {
+        color:darkcyan;
+    }
+</style>
