af66b3c57cef06cb89e54a1784b1c0b17587447d
Bram <brampijper@gmail.com>
2019-09-17 11:55:32 +0200

    included the uglifyjsplugin for minifying code in the production build

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,9 +1,12 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
     devtool: 'source-map',
     mode: 'production',
-
+    optimization: {
+        minimizer: [new UglifyJsPlugin()],
+    },
     module: {
         rules: [
             {
