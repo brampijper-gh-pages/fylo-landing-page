d5788c437c0d7375618ba139552e4696ebf5ea93
Bram <brampijper@gmail.com>
2019-09-26 17:00:47 +0200

    The navbar is responsive for desktop and mobile

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="main-nav">
-        <img src="https://picsum.photos/50/50" class="main-nav__logo" />
+        <img class="main-nav__logo" src="http://lorempixel.com/400/400" />
         <ul class="main-nav__menu">
             <li class="main-nav__menu-item">
                 <a class="main-nav__menu-item-link" href="#">
@@ -30,19 +30,21 @@
 .main-nav {
     display: grid;
     grid-template-columns: 1fr 10fr;
+    margin: 6vh 4vw 6vh 5vw;
 }
 
 .main-nav__logo {
-    flex-grow: 0;
-    width: 50px;
-    height: 50px;
+    width: auto;
+    height: 100%;
 }
 
 .main-nav__menu {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
     margin: 0;
     padding: 0;
     list-style-type: none;
-    text-align:right;
 }
 
 .main-nav__menu-item {
@@ -52,5 +54,28 @@
 
 .main-nav__menu-item-link {
     text-decoration: none;
+}
+
+/* only for smartphones */
+@media screen and (max-width: 480px) {
+    .main-nav {
+        grid-template-columns: 1fr;
+        grid-template-rows: 1fr 1.5fr;
+    }
+
+    .main-nav__menu {
+        text-align:center;
+        display: block;
+    }
+
+    .main-nav__menu-item {
+        display: block;
+        margin: 15px 0px 0px 0px;
+
+    }
+
+    .main-nav__logo {
+        margin: 0 auto;
+    }
 }
 </style>
