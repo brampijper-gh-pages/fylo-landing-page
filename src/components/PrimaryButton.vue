1b593cf77b5a6fa147bc78dc4a1302d4874e133b
Bram <brampijper@gmail.com>
2019-10-01 09:54:53 +0200

    Created a component for a re-usable button

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button>
+        Read more evidence
+    </button>
+</template>
+
+<script>
+export default {
+    //add props for button text so it's re-usable
+}
+</script>
+
+<style scoped>
+button {
+    width: 200px;
+    height: 50px;
+    border-radius: 15px;
+    border: 0px;
+    padding: 0px;
+}
+</style>
