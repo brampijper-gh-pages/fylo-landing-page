2579645989fd76a86aeccb4b67677941bcaa80d6
Bram <brampijper@gmail.com>
2019-10-04 19:58:36 +0200

    imported the nesessary libraries to make fortawesome work

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import './assets/css/global.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFacebookSquare, faInstagram, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faFacebookSquare, faInstagram, faTwitterSquare);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 new Vue({
     el: '#app',
     render: h => h(App)
