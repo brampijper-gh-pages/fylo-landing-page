e117ab7d1bb8c9c33d44ab6804d114ba02f51961
Bram <brampijper@gmail.com>
2019-10-04 17:08:04 +0200

    implemented basic animation on hover

--- src/components/PrimaryButton.vue
+++ src/components/PrimaryButton.vue
@@ -12,12 +12,19 @@ export default {
 
 <style scoped>
 button {
-    width: 200px;
-    height: 50px;
-    border-radius: 15px;
+    width: 190px;
+    height: 43px;
+    border-radius: 13px;
     border: 0px;
     padding: 0px;
     color: white; 
     background: linear-gradient(90deg, hsl(198, 60%, 50%) 50%, hsl(176, 68%, 64%) 105%);
+    transition: all .3s ease-out;
+}
+
+button:hover {
+    transform: scale(1.08);
+    cursor: pointer;
+    will-change: auto;
 }
 </style>
