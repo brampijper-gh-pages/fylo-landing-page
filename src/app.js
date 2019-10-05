906364789efa3d892f90c63023150e11cb020f87
Bram <brampijper@gmail.com>
2019-10-05 16:40:30 +0200

    imported specific icons from the fontawesome package

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ import './assets/css/global.css';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faFacebookSquare, faInstagram, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+import { faPhone, faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faFacebookSquare, faInstagram, faTwitterSquare);
+library.add(faFacebookSquare, faInstagram, faTwitterSquare, faPhone, faEnvelope, faMapMarkerAlt);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
