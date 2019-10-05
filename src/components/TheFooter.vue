46b01bc59a3251a279eba5cfb901a2fef85a4d65
Bram <brampijper@gmail.com>
2019-10-05 16:39:11 +0200

    Adjusted margin and changed some elements to make it more semantically

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,16 +2,30 @@
     <footer class="footer">
         <img class="footer__logo" src="assets/images/logo.svg" />
         <ul class="footer__footer-list">
-            <li>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem, vitae amet, modi,
-                cum impedit at enim ad dolores harum quis autem dignissimos quaerat.
+            <li class="footer-list__intro">
+                <span> 
+                    <font-awesome-icon icon="map-marker-alt" />
+                </span>
+                
+                <p class="paragraph--spacing">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem, vitae amet, modi,
+                    cum impedit at enim ad dolores harum quis autem dignissimos quaerat.
+                </p>
             </li>
 
-            <li>
-                <span> +31020 4930 49 </span>
-                <br />
-                <br />
-                <span> example@fylo.com </span>
+            <li class="footer-list__contact">
+                <article class="contact__details">
+                    <font-awesome-icon icon="phone" />
+                    <p class="paragraph--spacing">
+                        +3102049495 
+                    </p>
+                </article>
+                <article class="contact__details">
+                    <font-awesome-icon icon="envelope" />
+                    <p class="paragraph--spacing">
+                        exp@fylo.com 
+                    </p>
+                </article>
             </li>
 
             <ul class="footer-list__menu">
@@ -28,11 +42,18 @@
             </ul>
 
             <li class="footer-list__social">
-               <font-awesome-icon size="2x" :icon="[ 'fab', 'facebook-square' ]" />         
-               <font-awesome-icon size="2x" :icon="[ 'fab', 'twitter-square' ]" />         
-               <font-awesome-icon size="2x" :icon="[ 'fab', 'instagram' ]" />         
-            </li>
+                <a href="#">
+                    <font-awesome-icon class="social__icons" size="2x" :icon="[ 'fab', 'facebook-square' ]" />      
+                </a>   
 
+                <a href="#">
+                    <font-awesome-icon class="social__icons" size="2x" :icon="[ 'fab', 'twitter-square' ]" />         
+                </a>   
+
+                <a href="#">
+                    <font-awesome-icon class="social__icons" size="2x" :icon="[ 'fab', 'instagram' ]" />         
+                </a>    
+            </li>
         </ul>
 
     </footer>
@@ -46,16 +67,33 @@
 }
 
 .footer__logo {
-    margin-bottom: 15px;
+    margin-bottom: 30px;
     max-width: 175px;
 }
 .footer__footer-list {
     display: grid;
-    grid-template-columns: 2fr 1.5fr 1fr 1fr 1fr;
+    grid-template-columns: 2fr 1.5fr 1fr 1fr 1.5fr;
     gap: 5px;
     justify-items: center;
     list-style-type: none;
     padding: 0;
+}
+
+.footer-list__intro {
+    display:flex;
+    align-items:flex-start;
+}
+
+.paragraph--spacing {
+    display: inline;
+    margin: 0;
+    padding: 0;
+    margin-left: 10px;
+}
+
+.contact__details {
+    display:block;
+    margin-bottom: 20px;
 }
 
 .footer-list__menu {
@@ -73,16 +111,20 @@
     margin: 0 auto;
 }
 
-.footer-list__social svg {
-    margin: 0px 10px 0px 10px;
+.social__icons {
+    margin: 0px 0.8vw 0px 0.8vw;
 }
 
-@media screen and (max-width: 480px) {
+@media screen and (max-width: 600px) {
     .footer__footer-list {
         grid-template-columns: 1fr;
-        grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+        grid-template-rows: 0.7fr 1fr 1fr 1fr 1fr;
         justify-items: flex-start;
         align-items:center;
+    }
+
+    .social__icons {
+        margin: 0px 4vw 0px 4vw;
     }
 }
 </style>
