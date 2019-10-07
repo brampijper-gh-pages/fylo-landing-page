93a4ae0f3f6e07b95d4998806097a6dd30cf7a94
Bram <brampijper@gmail.com>
2019-10-07 10:18:26 +0200

    imported smoothscroll behaviour to support the browsers that haven't implemented the scrollTo function yet

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import './assets/css/global.css';
+import smoothscroll from 'smoothscroll-polyfill';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faFacebookSquare, faInstagram, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
@@ -10,6 +11,9 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faFacebookSquare, faInstagram, faTwitterSquare, faPhone, faEnvelope, faMapMarkerAlt);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+// to support auto scrolling on IE / Safari;
+smoothscroll.polyfill();
 
 new Vue({
     el: '#app',
