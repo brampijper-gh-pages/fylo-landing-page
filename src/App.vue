b0c85bd4cbc20ab73bf6e51695cf9c67791069d4
Bram <brampijper@gmail.com>
2019-10-08 21:09:43 +0200

    Implemented scrollTo function in the parent file so it's possible to call them through the child components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <the-navbar />
-        <base-content />
+        <the-navbar :scrollFunction="calculateScrollHeight"> </the-navbar>
+        <base-content :scrollFunction="calculateScrollHeight"> </base-content>
         <the-footer />
     </div>
 </template>
@@ -16,6 +16,24 @@ export default {
         TheNavbar,
         BaseContent,
         TheFooter
+    },
+    methods: {
+        calculateScrollHeight(scrollTo) {
+           const section = document.getElementsByClassName(scrollTo)
+
+            if(section.length) {
+                const offsetHeight = section[0].offsetHeight;
+                const offsetTop = section[0].offsetTop;
+                this.scrollToSection(offsetHeight + offsetTop)
+            }
+        },
+        scrollToSection(height) {
+            window.scroll({
+                top: height,
+                left: 0,
+                behavior: 'smooth'
+            });
+        }        
     }
 }
 </script>
